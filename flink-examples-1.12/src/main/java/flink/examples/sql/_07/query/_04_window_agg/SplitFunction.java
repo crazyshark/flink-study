078a0ee39e08191eebe8def6bede268b1b699185
crazyshark <shark.donald.zq@gmail.com>
2021-10-22 20:18:15 +0800

    代码修改

--- flink-examples-1.12/src/main/java/flink/examples/sql/_07/query/_04_window_agg/SplitFunction.java
+++ flink-examples-1.12/src/main/java/flink/examples/sql/_07/query/_04_window_agg/SplitFunction.java
@@ -7,13 +7,13 @@ import org.apache.flink.table.functions.TableFunction;
 import org.apache.flink.types.Row;
 import static org.apache.flink.table.api.Expressions.*;
 
-@FunctionHint(output = @DataTypeHint("ROW<word STRING, length INT>"))
+@FunctionHint(output = @DataTypeHint("ROW<word STRING>"))
 public class SplitFunction extends TableFunction<Row> {
 
     public void eval(String str) {
         for (String s : str.split(" ")) {
             // use collect(...) to emit a row
-            collect(Row.of(s, s.length()));
+            collect(Row.of(s));
         }
     }
 }
