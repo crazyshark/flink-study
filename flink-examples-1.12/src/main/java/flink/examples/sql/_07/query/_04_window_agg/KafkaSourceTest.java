721e11ab9e72f13f1b0644b069dc52f8e85d84a3
crazyshark <shark.donald.zq@gmail.com>
2021-11-02 21:26:13 +0800

    添加测试代码

---
+++ flink-examples-1.12/src/main/java/flink/examples/sql/_07/query/_04_window_agg/KafkaSourceTest.java
@@ -0,0 +1,45 @@
+package flink.examples.sql._07.query._04_window_agg;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+
+public class KafkaSourceTest {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        env.setParallelism(1);
+
+        EnvironmentSettings settings = EnvironmentSettings
+                .newInstance()
+                .useBlinkPlanner()
+                .inStreamingMode().build();
+
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env, settings);
+
+        tEnv.executeSql(
+                "CREATE TABLE KafkaSourceTable (\n"
+                        + "  `f0` STRING,\n"
+                        + "  `f1` STRING\n"
+                        + ") WITH (\n"
+                        + "  'connector' = 'kafka',\n"
+                        + "  'topic' = 'topic',\n"
+                        + "  'properties.bootstrap.servers' = 'localhost:9092',\n"
+                        + "  'properties.group.id' = 'testGroup',\n"
+                        + "  'format' = 'json'\n"
+                        + ")"
+        );
+
+        //Table t = tEnv.sqlQuery("SELECT * FROM KafkaSourceTable");
+
+        //tEnv.toAppendStream(t, Row.class).print();
+
+        //env.execute();
+        String sql = "SELECT * FROM KafkaSourceTable";
+        tEnv.explainSql(sql);
+    }
+}
