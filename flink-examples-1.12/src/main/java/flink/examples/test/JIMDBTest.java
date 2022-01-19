6c32fb0a07f9b800cbd639852b4ba5ac8680b88a
crazyshark <shark.donald.zq@gmail.com>
2022-01-19 11:13:54 +0800

    删除多余文件

--- flink-examples-1.12/src/main/java/flink/examples/test/JIMDBTest.java
+++
@@ -1,9 +0,0 @@
-package flink.examples.test;
-
-public class JIMDBTest {
-
-    public static void main(String[] args){
-        BaseCommandTest test = new BaseCommandTest();
-        test.testDestroy();
-    }
-}
--- flink-examples-1.12/src/main/java/flink/examples/test/TestConstanst.java
+++
@@ -1,6 +0,0 @@
-package flink.examples.test;
-
-public class TestConstanst {
-    public static final String STRING_KEY_HASH_TAG_1 ="JIMDBSTRINGKEYHASHTEST1116";
-    public static final String STRING_KEY_1 ="JIMDBSTRINGKEY1116";
-}
